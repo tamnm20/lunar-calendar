1d56870c13f1ca0f5bade014551088da2787ca25
tamnm20 <minhtam221220@gmail.com>
2026-01-22 12:55:50 +0700

    Add pwa

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "calendar-pwa-v1";
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./main.js",
+    "./lunar-calendar.js",
+    "./manifest.json",
+    "./calender_icon.png"
+];
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
+});
+self.addEventListener("activate", e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+        )
+    );
+});
